3995ebf8294f01a88a520a40da2da7960f1d86e5
James Clark <jdclark05@gmail.com>
2021-04-08 15:42:14 -0400

    py file edit

--- understanding-routing.py
+++ understanding-routing.py
@@ -22,39 +22,6 @@ def repeat(num,word):
     return newWord
     return render_template("repeat.html", word=newWord)
 
-# @app.route("/say/flask")
-# def say_flask():
-#     return render_template("say_flask.html")
-
-# @app.route("/say/michael")
-# def say_michael():
-#     return render_template("say_michael.html")
-
-# @app.route("/say/john")
-# def say_john():
-#     return render_template("say_john.html")
-
-# @app.route("/others")
-# def others():
-#     db_others = [
-#         {"first_name":"Tom","last_name":"Tom","email":"ttom@python.com"},
-#         {"first_name":"Dick","last_name":"Johnson","email":"djohnson@python.com"},
-#         {"first_name":"Harry","last_name":"Balderdash","email":"hbalderdash@python.com"},
-#         {"first_name":"Jerry","last_name":"Jones","email":"jjones@python.com"}
-#     ]
-#     return render_template("others.html", users=db_others)
-
-# @app.route("/add/user")
-# def add_user():
-#     return render_template("add_user.html")
-
-# @app.route("/create/user",methods=['POST'])
-# def create_user():
-#     print(request.form)
-#     print(request.form['first_name'])
-#     print(request.form['last_name'])
-#     print(request.form['email'])
-
 if __name__=="__main__":
     # Method that starts our flask server
     # We give it an argument to debug
